307f4e79c6f941648f4b1459200623b4566bacb7
mey.beisaron <mey.beisaron@appsflyer.com>
2020-04-05 23:20:12 +0300

    added movement in Y axis added collision handling , added screen borders detection

--- server/main.go
+++ server/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	_ "encoding/json"
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/gorilla/websocket"
 	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
 	"sync"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
 
 // Define our message object
@@ -19,29 +20,36 @@ type actionMessage struct {
 	Y string // "0" || "10" ||  "-10"
 }
 
-type point struct{
-	X   int64    `json:"x"`
-	Y int64     `json:"y"`
+type screenWH struct {
+	Width int64 // "0" || "10" ||  "-10"
+	Height int64 // "0" || "10" ||  "-10"
+}
+
+type point struct {
+	X int64 `json:"x"`
+	Y int64 `json:"y"`
 }
 
 type Player struct {
-	Id uuid.UUID `json"id"`
-	ExceptionType string `json:"exceptionType"` //add id : uuid
-	Color [3]int `json:"color"`
-	Size int `json:"size"`
-	P point `json:"p"`
-	Show bool `json:"show"`
-
+	Id            uuid.UUID `json"id"`
+	ExceptionType string    `json:"exceptionType"`
+	Color         [3]int    `json:"color"`
+	P             point     `json:"p"`
+	Show          bool      `json:"show"`
+	windowH 	 int64
+	windowW      int64
+	Collision   string      `json:"collision"`  // border || player || exception
+	Score       int   `json:"score"`
 }
 
 type Exception struct {
-	Id uuid.UUID `json"id"`
-	ExceptionType string `json:"exceptionType"`
-	Show bool `json:"show"`
-	P point `json:"p"`
+	Id            uuid.UUID `json"id"`
+	ExceptionType string    `json:"exceptionType"`
+	Show          bool      `json:"show"`
+	P             point     `json:"p"`
 }
 
-var exceptionsMap = struct{
+var exceptionsMap = struct {
 	sync.RWMutex
 	m map[point]Exception
 }{m: make(map[point]Exception)}
@@ -53,33 +61,72 @@ var broadcastPlayers = make(chan Player)
 var broadcastException = make(chan Exception)
 var upgrader = websocket.Upgrader{}
 
-
 func handleNewPlayer(ws *websocket.Conn) {
 	r2 := rand.New(s)
-	player := Player{ Id:uuid.New(),  P:point{X:0, Y:0}, Size: 50, Show:true, ExceptionType: exceptionsTypes[rand.Intn(3)], Color:[3]int{r2.Intn(256), r2.Intn(256), r2.Intn(256)} }
+	player := Player{Id: uuid.New(), P: point{X: int64(r2.Intn(300)), Y: 0}, Score: 0, Show: true, ExceptionType: exceptionsTypes[rand.Intn(3)], Color: [3]int{r2.Intn(256), r2.Intn(256), r2.Intn(256)}, Collision: "" }
 
-
-	broadcastPlayers <- player //broadcast new player
+	fmt.Println("new player")
+	fmt.Println(player)
+	ws.WriteJSON(player)
 
 	//send to new player all current players
-
 	for key := range clients {
 		err := ws.WriteJSON(*clients[key])
 		if err != nil {
-			log.Printf("error: %v", err)
+			log.Printf( "76 error: %v", err)
 			ws.Close()
 			delete(clients, ws)
 		}
 	}
 
 	//TODO send to new player all current exceptions
-
-
 	clients[ws] = &player
+	broadcastPlayers <- player //broadcast new player
 
-	fmt.Println("new player")
-	fmt.Println( *clients[ws])
+	var msg screenWH
+	err := ws.ReadJSON(&msg)
+	if err != nil {
+		log.Printf(" 89 error: %v", err)
+		var plyrMsg = clients[ws]
+		plyrMsg.Show = false
+		broadcastPlayers <- *plyrMsg
+		delete(clients, ws)
+	}
+	clients[ws].windowH = msg.Height
+	clients[ws].windowW = msg.Width
+
 }
+
+func handlePlayerMovement(ws *websocket.Conn, newX int64, newY int64){
+
+	x := int64(clients[ws].P.X) + newX
+	y := int64(clients[ws].P.Y) + newY
+	player := *clients[ws]
+
+	if y < 0  || x < 0 || x >= clients[ws].windowW || y >= clients[ws].windowH{
+		player.Collision = "border"
+	}else{
+
+		//check for collision with other players
+		for key := range clients {
+			client := *clients[key]
+			if client.P.X == x ||  client.P.Y == y{
+				player.Collision = "player"
+			}
+		}
+
+		//TODO add check collisions with exceptions
+		player.P.X = x
+		clients[ws].P.X = x
+		clients[ws].P.Y = y
+		player.P.Y = y
+		fmt.Println("player with new values ")
+		fmt.Println(*clients[ws])
+	}
+
+	broadcastPlayers <- player
+}
+
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
 
@@ -94,33 +141,22 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 
 	defer ws.Close() // Make sure we close the connection when the function returns
 
-
 	for {
 		var msg actionMessage
 		// Read in a new message as JSON and map it to a incomingMessage object
 		err := ws.ReadJSON(&msg)
 		if err != nil {
-			log.Printf("error: %v", err)
-			var plyrMsg =  clients[ws]
-			plyrMsg.Show= false;
+			log.Printf(" 149 error: %v", err)
+			var plyrMsg = clients[ws]
+			plyrMsg.Show = false
 			broadcastPlayers <- *plyrMsg
 			delete(clients, ws)
 			break
 		}
-		fmt.Println("received msg:")
-		fmt.Println(msg)
-
-		fmt.Println("player curr values")
-		fmt.Println(clients[ws])
 		//INSERT HERE  call to check collision function
-		newX, _ :=  strconv.ParseInt(msg.X, 10, 64)
-		clients[ws].P.X = int64(clients[ws].P.X) + newX
-
-	//	newY, _ :=  strconv.ParseInt(msg.y, 10, 64)
-	//	clients[ws].p.y = int64(clients[ws].p.y) + newY
-		fmt.Println("player with new values ")
-		fmt.Println(*clients[ws])
-		broadcastPlayers <- *clients[ws]
+		newX, _ := strconv.ParseInt(msg.X, 10, 64)
+		newY, _ := strconv.ParseInt(msg.Y, 10, 64)
+		handlePlayerMovement(ws, newX , newY)
 	}
 }
 func broadcastMessages() {
@@ -132,7 +168,7 @@ func broadcastMessages() {
 		for client := range clients {
 			err := client.WriteJSON(msg)
 			if err != nil {
-				log.Printf("error: %v", err)
+				log.Printf("171 error: %v", err)
 				client.Close()
 				delete(clients, client)
 			}
@@ -141,21 +177,19 @@ func broadcastMessages() {
 	}
 }
 
-
-
-func exceptiosMapHandler (){
+func exceptiosMapHandler() {
 	//Thread.
 	// every 5 sec:  create new ex ->add to eXarr -> broadcast to users
 	//exery 10 sec:  choose rand ex ->remove from exArr ->broadcast to users
 
 	var r = rand.New(s)
-	addExTicker := time.NewTicker(5* time.Second)
+	addExTicker := time.NewTicker(5 * time.Second)
 	go func() {
 		for t := range addExTicker.C {
 			_ = t // we don't print the ticker time, so assign this `t` variable to underscore `_` to avoid error
-			var newEx = Exception{ Id:uuid.New(), ExceptionType: exceptionsTypes[r.Intn(3)], P:point{X:int64(r.Intn(255)), Y:int64(r.Intn(255))}, Show:true }
+			var newEx = Exception{Id: uuid.New(), ExceptionType: exceptionsTypes[r.Intn(3)], P: point{X: int64(r.Intn(255)), Y: int64(r.Intn(255))}, Show: true}
 			exceptionsMap.Lock() //take the write lock
-			exceptionsMap.m[newEx.P]=newEx;
+			exceptionsMap.m[newEx.P] = newEx
 			exceptionsMap.Unlock() // release the write lock
 			fmt.Println(newEx)
 			broadcastException <- newEx
@@ -163,7 +197,7 @@ func exceptiosMapHandler (){
 		}
 	}()
 
-	removeExTicker := time.NewTicker(10* time.Second)
+	removeExTicker := time.NewTicker(10 * time.Second)
 	go func() {
 		for t := range removeExTicker.C {
 			_ = t // we don't print the ticker time, so assign this `t` variable to underscore `_` to avoid error
@@ -174,7 +208,7 @@ func exceptiosMapHandler (){
 				exceptionsMap.RUnlock()
 				break
 			}
-			value.Show =false
+			value.Show = false
 			broadcastException <- value
 			//exceptionsMap.Lock() //take the write lock
 			//delete(exceptionsMap, value.p)
@@ -184,15 +218,11 @@ func exceptiosMapHandler (){
 		}
 	}()
 
-
 	// wait for 10 seconds
 	//time.Sleep(20 *time.Second)
-//	ticker.Stop()
-
+	//	ticker.Stop()
 
 }
-
-
 
 func main() {
 	fmt.Println("websockets project")
--- src/App.js
+++ src/App.js
@@ -1,83 +1,98 @@
 
-    import React, { useEffect, useState, useRef } from 'react';
-    import './App.css';
-    import monkey from './monkey.png'
-    import banana from './banana.png'
+import React, { useEffect, useState, useRef, useLayoutEffect } from 'react';
+import './App.css';
+import monkey from './monkey.png'
+const socket = new WebSocket('ws://localhost:8080/ws');
+const MONKEYWIDTH = 100;
+const MONKEYHEIGHT = 129;
+const STEP = 10;
+const NOSTEP = 0
 
-    
-    
-    const socket = new WebSocket('ws://localhost:8080/ws');
-    
-    
-    
-    
-    function App() {
-    
-      const [players, setPlayers] = useState(["roei","mey"]);
-      const [location, settLocation] = useState([
-         {
-          y: "0", x: "0"
-         }
-        ]
-      );
+function App() {
+  const [players, setPlayers] = useState([]);
+  const bodyBoundries = useRef(null);
 
-      // const isFirstRun = useRef(true);
-    
-      function showKeyCode(e) {
-        //right
-        if (e.keyCode === 39) {
-          socket.send(JSON.stringify({y:"0",x:"10"}))
-        }
-        //left
-        if (e.keyCode === 37) {
-          socket.send(JSON.stringify({y:"0",x:"-10"}))
-        }
-    
-    
-      }
-      useEffect(() => {
-        document.body.addEventListener('keyup', showKeyCode);
-      }, [])
-    
-    
-
-    
-    
-      useEffect(() => {
-    
-        socket.onmessage = (event) => {
-           const parseData = JSON.parse(event.data)
-           console.log("TCL: socket.onmessage -> parseData", parseData.p)
-        
-         
-          
-          settLocation([parseData.p])
-
-        }
-        socket.onopen = () => {
-          console.log("connected successfuly")
-        }
-    
-        socket.onclose = (e) => {
-          console.log("socket close connection", e)
-        }
-    
-    
-        socket.onerror = (e) => {
-          console.log("socket error", e)
-        }
-      }, []);
-    
-    
-      return (
-        <>
-         
-          <img className="monkey"  style={{ left: location[0].x + 'px' }} src={monkey}  />
-    
-        </>
-      );
+  function showKeyCode(e) {
+    if (e.key === 'ArrowRight') {
+      socket.send(JSON.stringify({ y: `${NOSTEP}`, x: `${STEP}` }))
     }
-    
-    export default App;
-    
+    if (e.key === 'ArrowLeft') {
+      socket.send(JSON.stringify({ y: `${NOSTEP}`, x: `-${STEP}` }))
+    }
+    if (e.key === 'ArrowUp') {
+      socket.send(JSON.stringify({ y: `${STEP}`, x: `${NOSTEP}` }))
+    }
+    if (e.key === 'ArrowDown') {
+      socket.send(JSON.stringify({ y: `-${STEP}`, x: `${NOSTEP}` }))
+    }
+  }
+  useEffect(() => {
+    document.body.addEventListener('keydown', showKeyCode);
+    return () => {
+      document.body.removeEventListener('keydown', showKeyCode);
+    };
+  }, [])
+
+  useLayoutEffect(() => {
+    const getBodyBoundries = document.body.getBoundingClientRect();
+    bodyBoundries.current = {
+      width: (getBodyBoundries.width - MONKEYWIDTH),
+      height: (getBodyBoundries.height - MONKEYHEIGHT)
+    }
+  }, [])
+
+  useEffect(() => {
+    const onMessage = event => {
+      const parseData = JSON.parse(event.data);
+      setPlayers(currPlayers => {
+        const objIndex = currPlayers.findIndex(obj => obj.Id === parseData.Id);
+        if (objIndex !== -1) {
+          const clonePlayers = JSON.parse(JSON.stringify(currPlayers));
+          if(parseData.show === false){
+            const filtered = clonePlayers.filter(player=> player.Id !== parseData.Id); 
+            return filtered;
+          } 
+          clonePlayers[objIndex].p = parseData.p;
+          return clonePlayers;
+        }
+        return [...currPlayers, parseData];
+      });
+    };
+
+    socket.addEventListener('message', onMessage);
+
+    return () => socket.removeEventListener('message', onMessage);
+  },[players]);
+
+
+
+  useEffect(() => {
+    socket.onopen = () => {
+      socket.send(JSON.stringify(bodyBoundries.current))
+    }
+    return () => {
+      socket.onclose = (e) => {
+        console.log("socket close connection", e)
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      {players.map(player => {
+        return (
+          <div key={player.Id}>
+              <div className="plaeyrImg" style={{ position: 'absolute', right: 0, bottom: player.p.y + 'px', left: player.p.x + 'px', color: `rgb(${player.color[0]},${player.color[1]},${player.color[2]})` }}>
+                <span style={{ position: "absolute", top: "-40px", left: "-25px" }}>{player.exceptionType}</span>
+                <img src={monkey} />
+              </div>
+          </div>
+        )
+      })}
+    </>
+  );
+}
+
+export default App;
+
 
